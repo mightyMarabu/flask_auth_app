eb3c60919688bbeed3e26768da213a084902ce54
sebastian <sebastian.schmidt@ot-movimento.de>
2020-11-16 13:49:56 +0100

    flask_auth app up and running

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -54,6 +55,7 @@ def signup_post():
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
 
 
